e18587d9515de8931e04558c7b15853e2bb2cc73
Yuriy Yaremkiv <y.yaremkiv@gmail.com>
2022-09-29 21:27:44 +0300

    add task 10

---
+++ js/task10.js
@@ -0,0 +1,35 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+    input: document.querySelector('[type="number"]'),
+    createButton: document.querySelector('[data-create]'),
+    destroyButton: document.querySelector('[data-destroy]'),
+    box: document.querySelector('#boxes'),
+}
+
+refs.createButton.addEventListener('click', () => {
+    refs.box.innerHTML = '';
+    let size = 30
+
+    for (i = 1; i <= refs.input.value; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.style.backgroundColor = getRandomHexColor();
+        newDiv.style.width = `${size}px`;
+        newDiv.style.height = `${size}px`;
+        refs.box.append(newDiv);
+
+        size += 10;
+    }
+
+    refs.input.value = '';
+});
+
+refs.destroyButton.addEventListener('click', () => {
+    refs.box.innerHTML = '';
+    refs.input.value = '';
+});
+

b2f138ae69cdfcce99814127cb0593defd9faa1a
Yuriy Yaremkiv <y.yaremkiv@gmail.com>
2022-09-30 14:27:19 +0300

    update task 10

--- js/task10.js
+++ js/task10.js
@@ -15,16 +15,21 @@ refs.createButton.addEventListener('click', () => {
     refs.box.innerHTML = '';
     let size = 30
 
+    //Новий div, щоб не вставляти в DOM окремо каждий блок в циклі
+    const generalDiv = document.createElement('div');
+    generalDiv.classList.add('box');
+
     for (i = 1; i <= refs.input.value; i++) {
         const newDiv = document.createElement('div');
         newDiv.style.backgroundColor = getRandomHexColor();
         newDiv.style.width = `${size}px`;
         newDiv.style.height = `${size}px`;
-        refs.box.append(newDiv);
+        generalDiv.append(newDiv);
 
         size += 10;
     }
 
+    refs.box.append(generalDiv);
     refs.input.value = '';
 });
 
